6830a23ed4abb21850d9762e6e3c536e83bb97d5
Benjamin Hsieh <bhsiehh@gmail.com>
2012-03-11 19:36:32 -0700

    created app

---
+++ ICA/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ICA::Application.config.secret_token = 'c9724e62f9cb4013454467a431fc2384ef09aa8d02a522e9946a687a3d1a189d176b8f10e477c04e1488f9d4ce8ddd8de336c82143720714d2f133421ee61279'
