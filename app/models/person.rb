b1348f613e9f522012f51cd2128800794b7bd11a
Tom Liao <vicros@berkeley.edu>
2012-03-27 19:33:55 -0700

    Created factories and some basic models using polymorphism and single table inheritance

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,10 @@ class Person < ActiveRecord::Base
   has_many :phone_numbers, :as => :contactable, :class_name => 'PhoneNumber'
   has_many :physical_addresses, :as => :contactable, :class_name => 'PhysicalAddress'
   has_many :email_addresses, :as => :contactable, :class_name => 'EmailAddress'
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :occupation
 end
 
 class Individual < Person
