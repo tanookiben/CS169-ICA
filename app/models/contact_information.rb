388696896be5c979b7187cb4c0f078a5973b02cc
Tom Liao <vicros@berkeley.edu>
2012-04-01 01:23:35 -0700

    Removed separate search path. Working on linking to a new contact page. Pulled some models out of the same file for single table inheritance because keeping them all in contact_information.rb was causing problems. Added a migration to put phone numbers in the database

--- app/models/contact_information.rb
+++ app/models/contact_information.rb
@@ -1,12 +1,3 @@
 class ContactInformation < ActiveRecord::Base
   belongs_to :contactable, :polymorphic => true
 end
-
-class PhoneNumber < ContactInformation
-end
-
-class PhysicalAddress < ContactInformation
-end
-
-class EmailAddress < ContactInformation
-end
---
+++ app/models/email_address.rb
@@ -0,0 +1,2 @@
+class EmailAddress < ContactInformation
+end
