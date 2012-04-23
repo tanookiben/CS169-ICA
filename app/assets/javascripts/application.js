6c5d5318af4863e7666db4fab7807fb1051b0cd4
Tom Liao <vicros@berkeley.edu>
2012-04-22 19:34:12 -0700

    Extracted javascript to a separate file. Refactored some files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,13 +8,3 @@
 //= require jquery_ujs
 //= require_tree .
 
-function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val(true);
-  $(link).closest('.fields').hide();
-}
-
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
-  $(link).before(content.replace(regexp, new_id));
-}
