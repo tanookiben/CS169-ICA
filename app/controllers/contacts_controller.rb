cf6a5b2f8221e0692c32583e150e26e959150c92
Tom Liao <vicros@berkeley.edu>
2012-04-27 15:38:12 -0700

    Tried to fix the rake db issue with the specs, but was unable. It has something to do with how the companies factory file creates new individuals. Set business to be the default selection for the emails and phone numbers. Added 'New Person' and 'New Company' selections to the header and made the header floating. Refactored some code in the controllers

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,17 +11,11 @@ class ContactsController < ApplicationController
 
   def search
     @search_term = params[:search]
-    @individuals = @search_term ? Individual.search(@search_term) : Individual.all
-    @individuals = @individuals.paginate(page: params[:individuals_page], per_page: 10)
-    @board_members = @search_term ? BoardMember.search(@search_term) : BoardMember.all
-    @board_members = @board_members.paginate(page: params[:board_members_page], per_page: 10)
-    @advisors = @search_term ? Advisor.search(@search_term) : Advisor.all
-    @advisors = @advisors.paginate(page: params[:advisors_page], per_page: 10)
-    @education_companies = @search_term ? EducationCompany.search(@search_term) : EducationCompany.all
-    @education_companies = @education_companies.paginate(page: params[:education_companies_page], per_page: 10)
-    @portfolio_companies = @search_term ? PortfolioCompany.search(@search_term) : PortfolioCompany.all
-    @portfolio_companies = @portfolio_companies.paginate(page: params[:portfolio_companies_page], per_page: 10)
-    @professional_service_providers = @search_term ? ProfessionalServiceProvider.search(@search_term) : ProfessionalServiceProvider.all
-    @professional_service_providers = @professional_service_providers.paginate(page: params[:professaionl_service_providers_page], per_page: 10)
+    @individuals = Individual.search(@search_term).paginate(page: params[:individuals_page], per_page: 10)
+    @board_members = BoardMember.search(@search_term).paginate(page: params[:board_members_page], per_page: 10)
+    @advisors = Advisor.search(@search_term).paginate(page: params[:advisors_page], per_page: 10)
+    @education_companies = EducationCompany.search(@search_term).paginate(page: params[:education_companies_page], per_page: 10)
+    @portfolio_companies = PortfolioCompany.search(@search_term).paginate(page: params[:portfolio_companies_page], per_page: 10)
+    @professional_service_providers = ProfessionalServiceProvider.search(@search_term).paginate(page: params[:professaionl_service_providers_page], per_page: 10)
   end
 end
--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,22 +1,24 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require Rails.root.join("spec/factories/people.rb")
+
 FactoryGirl.define do
   factory :educ, class: 'EducationCompany' do
     type "EducationCompany"
     name "MyEducationCompany"
-    representative = Person.new({ :first_name => "MyFirst", :last_name => "MyLast", :occupation => "MyJob", :type => "Individual" })
+    representative = FactoryGirl.create(:individual)
     representative_role "Education"
   end
   factory :prtf, class: 'PortfolioCompany' do
     type "PortfolioCompany"
     name "MyPortfolioCompany"
-    representative = Person.new({ :first_name => "MyFirst", :last_name => "MyLast", :occupation => "MyJob", :type => "Individual" })
+    representative = FactoryGirl.create(:individual, { :first_name => "MyFirst", :last_name => "MyLast", :occupation => "MyJob"})
     representative_role "Portfolio"
   end
   factory :prof, class: 'ProfessionalServiceProvider' do
     type "ProfessionalServiceProvider"
     name "MyProfessionalCompany"
-    representative = Person.new({ :first_name => "MyFirst", :last_name => "MyLast", :occupation => "MyJob", :type => "Individual" })
+    representative = FactoryGirl.create(:individual, { :first_name => "MyFirst", :last_name => "MyLast", :occupation => "MyJob"})
     representative_role "Professional"
   end
 end
