00e23aefffb078fa0a395288092f9de410683bcd
Tom Liao <vicros@berkeley.edu>
2012-04-29 05:15:59 -0700

    The autocomplete with Bootstrap typeahead works. Still don't have validations so badly specified representative names don't throw errors like they should. For example, if someone typed just the letter a into the representative name field, it does not say 'This representative is not a valid individual'; instead, it will simply return the first individual with an a in their first or last name. This is a bug and should be fixed. Should go through and work out all the validations properly

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,10 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new
+    if params[:company][:representative_name].blank?
+      flash[:error] = "Please include a representative."
+      redirect_to new_company_path and return
+    end
     params[:company] = params[:company].merge({:representative_id => Person.search(params[:company][:representative_name]).first.id}).except(:representative_name)
     if @company.update_with(params[:company])
       flash[:notice] = "#{@company.name} was created."
@@ -14,10 +18,12 @@ class CompaniesController < ApplicationController
 
   def new
     @company = Company.new
+    @representatives = Individual.all.collect{ |i| i.name }
   end
 
   def edit
     @company = Company.find(params[:id])
+    @representatives = Individual.all.collect{ |i| i.name }
   end
 
   def show
@@ -26,13 +32,18 @@ class CompaniesController < ApplicationController
 
   def update
     @company = Company.find(params[:id])
+    if params[:"#{@company.type.underscore}"][:representative_name].blank?
+      flash[:error] = "Please include a representative."
+      redirect_to edit_company_path(@company) and return
+    end
     params[:"#{@company.type.underscore}"] = params[:"#{@company.type.underscore}"].merge({:representative_id => Person.search(params[:"#{@company.type.underscore}"][:representative_name]).first.id}).except(:representative_name)
     if @company.update_with(params[:"#{@company.type.underscore}"])
       flash[:notice] = "#{@company.name} was updated."
+      redirect_to company_path(@company)
     else
       flash[:error] = "Cannot update #{@company.name}. #{@company.errors.full_messages}"
+      redirect_to edit_company_path(@company)
     end
-    redirect_to company_path(@company)
   end
 
   def destroy
