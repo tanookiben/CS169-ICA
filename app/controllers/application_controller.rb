ae6cfc9d206e07dee72c8d513d4b5c2d8558e447
Benjamin Hsieh <bhsiehh@gmail.com>
2012-05-02 14:07:28 -0700

    minor fix to current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
   end
 end
